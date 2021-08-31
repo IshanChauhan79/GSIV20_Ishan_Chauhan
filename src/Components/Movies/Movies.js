f03488716b29f04e3e1f157277849136842b7ed0
ishan <ishan.hcl.tab1@gmail.com>
2021-09-01 00:59:55 +0530

    Add movie card

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -2,10 +2,101 @@ import React from "react";
 import MovieCard from "../UI/SearchBox/MovieCard/MovieCard";
 import classes from "./Movies.module.css";
 
+const img = "https://image.tmdb.org/t/p/w500";
+const data = {
+  adult: false,
+  backdrop_path: "/w2PMyoyLU22YvrGK3smVM9fW1jj.jpg",
+  genre_ids: [28, 12, 878],
+  id: 299537,
+  original_language: "en",
+  original_title: "Captain Marvel",
+  overview:
+    "The story follows Carol Danvers as she becomes one of the universe’s most powerful heroes when Earth is caught in the middle of a galactic war between two alien races. Set in the 1990s, Captain Marvel is an all-new adventure from a previously unseen period in the history of the Marvel Cinematic Universe.",
+  popularity: 196.916,
+  poster_path: "/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg",
+  release_date: "2019-03-06",
+  title: "Captain Marvel",
+  video: false,
+  vote_average: 6.9,
+  vote_count: 11846,
+};
+
 function Movies() {
   return (
     <div className={classes.Movies}>
-      <MovieCard />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
+      <MovieCard
+        img={img + data.poster_path}
+        title={data.original_title}
+        desc={data.overview}
+        rate={data.vote_average}
+      />
     </div>
   );
 }
--- src/Components/UI/SearchBox/MovieCard/MovieCard.js
+++ src/Components/UI/SearchBox/MovieCard/MovieCard.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import classes from  './MovieCard.module.css';
+import React from "react";
+import classes from "./MovieCard.module.css";
 
-function MovieCard() {
+function MovieCard(props) {
   return (
     <div className={classes.MovieCard}>
-      hello
+      <div
+        style={{ backgroundImage: `url(${props.img})` }}
+        className={classes.Img}
+      ></div>
+      {/* <img src={props.img} /> */}
+      <div className={classes.Details}>
+        <div className={classes.TitleRow}>
+          <div className={classes.Title}>{props.title}</div>
+          <div className={classes.Rating}>
+            <img src="https://img.icons8.com/ios-glyphs/16/fbbc04/star--v1.png" alt="" /> 
+            {props.rate}
+          </div>
+        </div>
+
+        <div className={classes.Desc}>{props.desc.substring(0, 60)}...</div>
+      </div>
     </div>
   );
 }
